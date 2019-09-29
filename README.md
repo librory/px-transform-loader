7121901d716759c5adfcd707d9b0215de50be0b7
晴风 <289651189@qq.com>
2019-09-29 10:35:32 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,32 @@
+# 简介
+
+将 css 中 px 转换为 rem 的 loader
+
+# 使用方法
+
+```js
+const pxTransformLoader = require('px-transform-loader')
+
+module: {
+  rules: [
+    {
+      test: /\.css$/,
+      loader: pxTransformLoader,
+      options: {
+        rootPx: 12,
+        precision: 4,
+        roundingMethod: 'round', //round:四舍五入 enter:进一
+        min: 2
+      }
+    }
+  ]
+}
+```
+
+# 参数说明
+
+参数名 | 作用 | 默认值
+rootPx | 设置根元素的px值 | 12 |
+precision | 精度 | 4 |
+roundingMethod | 舍入方法（round：四舍五入；enter：进一） | round |
+min | 最小转换值（类似于1px的border，一般不希望被转换） | 2 |
---
+++ index.js
@@ -0,0 +1,52 @@
+var css = require('css')
+var getOptions = require('loader-utils').getOptions
+var validateOptions = require('schema-utils')
+
+const defaultOptions = {
+  rootPx: 12,
+  precision: 4,
+  roundingMethod: 'round', //round:四舍五入 enter:进一
+  min: 2
+}
+
+const _replaceCb = (value, { rootPx, precision, roundingMethod, min } = {}) => {
+  console.log('origin:', value)
+  if (value < min) return value + 'px'
+  const roundFn = roundingMethod === 'enter' ? Math.ceil : Math.floor
+  const coeffient = Math.pow(10, precision)
+
+  const ret = roundFn((value / rootPx) * coeffient) / coeffient + 'rem'
+
+  console.log('result:', ret)
+
+  return ret
+}
+
+const schema = {
+  type: 'object',
+  properties: {
+    rootPx: { type: 'number' },
+    precision: { type: 'number' },
+    roundingMethod: { type: 'string' },
+    min: { type: 'number' }
+  }
+}
+
+module.exports = function(source) {
+  const inputOptions = getOptions(this) || {}
+
+  validateOptions(schema, inputOptions)
+
+  const options = { ...defaultOptions, ...inputOptions }
+
+  let ast = css.parse(source)
+  ast.stylesheet.rules.forEach(rule => {
+    if (rule.type !== 'rule') return
+    rule.declarations.forEach(declaration => {
+      declaration.value = declaration.value.replace(/([\d\.]+)px/, (...item) =>
+        _replaceCb(Number(item[1]), options)
+      )
+    })
+  })
+  return css.stringify(ast)
+}
